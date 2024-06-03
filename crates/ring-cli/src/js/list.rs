762968dc085328653b4a4a66692d3db0ba5f2a74
Julien Capellari <julien.capellari@gmail.com>
2024-06-03 22:57:05 +0200

    feat: search project root algorithm

--- crates/ring-cli/src/js/list.rs
+++ crates/ring-cli/src/js/list.rs
@@ -1,8 +1,8 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::{arg, ArgMatches, Command, value_parser};
 use std::env;
-use std::path::PathBuf;
-use tracing::info;
+use std::path::{Path, PathBuf};
+use tracing::{debug, info, trace, warn};
 
 pub fn build_command() -> Command {
     Command::new("list")
@@ -20,14 +20,49 @@ pub fn handle_command(args: &ArgMatches) -> Result<()> {
     let project_dir = args.get_one::<PathBuf>("project")
         .unwrap_or(&current_dir);
     
-    let project_dir = if project_dir.is_absolute() { 
-        project_dir.clone()
-    } else {
-        current_dir.join(project_dir)
-    };
+    let project_dir = current_dir.join(project_dir).canonicalize()
+        .context(format!("Unable to access {}", project_dir.display()))?;
     
-    let project_dir = project_dir.canonicalize()?;
-    info!("project directory: {}", project_dir.display());
+    // Search project root
+    info!("Searching project root from {}", project_dir.display());
+    let mut root = if project_dir.is_file() { project_dir.parent().unwrap() } else { &project_dir };
+    let mut manifest = None;
     
+    loop {
+        trace!("Testing {}", root.display());
+        
+        for lockfile in ["package-lock.json", "yarn.lock"] {
+            let lockfile = root.to_path_buf().join(lockfile);
+            
+            if lockfile.try_exists().context(format!("Unable to access {}", lockfile.display()))? {
+                debug!("Found lockfile {}", lockfile.display());
+                info!("Project root is {}", root.display());
+                return Ok(());
+            }
+        }
+
+        {
+            let file = root.to_path_buf().join("package.json");
+            
+            if file.try_exists().context(format!("Unable to access {}", file.display()))? {
+                debug!("Found manifest {}", file.display());
+                manifest = Some(file);
+            }
+        }
+        
+        // Move up
+        if let Some(parent) = root.parent() {
+            root = parent;
+        } else {
+            break;
+        }
+    }
+    
+    if let Some(root) = manifest.as_deref().and_then(Path::parent) {
+        info!("Project root is {}", root.display());
+        return Ok(());
+    }
+    
+    warn!("Project root not found");
     Ok(())
 }
