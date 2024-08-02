f4754493c86efe6408167536c7b770da7530f925
Julien Capellari <julien.capellari@gmail.com>
2024-08-02 18:03:09 +0200

    chore: clean legacy code

--- crates/ring-js-project/src/constants.rs
+++
@@ -1,7 +0,0 @@
-use crate::PackageManager;
-
-pub const MANIFEST: &str = "package.json";
-pub const LOCKFILES: [(PackageManager, &str); 2] = [
-    (PackageManager::NPM, "package-lock.json"),
-    (PackageManager::Yarn, "yarn.lock")
-];
