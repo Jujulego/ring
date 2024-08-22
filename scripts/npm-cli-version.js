298e07a7d557840e527c4ed57f48028fcae25027
Julien Capellari <julien.capellari@gmail.com>
2024-08-22 15:40:24 +0200

    Deploy to npm (#56)
    * chore: add npm ring package (inspired by turbo's one)
    
    * chore: add script generating native packages
    
    * chore: add publish npm packages
    
    * chore: self review

---
+++ scripts/npm-cli-version.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const version = process.argv[2]?.replace(/^v/, '') ?? '0.0.0-dev';
+const packages = ['ring'];
+
+while (packages.length) {
+  const name = packages.pop();
+
+  const manifestPath = path.resolve(__dirname, '..', 'npm', name, 'package.json');
+  const manifest = require(manifestPath);
+
+  manifest.version = version;
+
+  if (manifest.optionalDependencies) {
+    for (const dep in manifest.optionalDependencies) {
+      manifest.optionalDependencies[dep] = version;
+      packages.push(dep.replace(/^@jujulego\//, ''));
+    }
+  }
+
+  fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+}
---
+++ scripts/npm-native-packages.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const template = require('./templates/package.tpl.json');
+
+// Map to node os and arch names.
+const targets = [
+  { platform: 'linux', arch: 'amd64' },
+  { platform: 'windows', arch: 'amd64' },
+  { platform: 'macos', arch: 'amd64' },
+  { platform: 'macos', arch: 'arm64' },
+];
+
+const nodeOsLookup = {
+  linux: 'linux',
+  windows: 'win32',
+  macos: 'darwin',
+};
+
+const nodeArchLookup = {
+  amd64: 'x64',
+  arm64: 'arm64',
+};
+
+const humanizedArchLookup = {
+  amd64: 'x64',
+  arm64: 'arm64',
+};
+
+for (const { platform, arch } of targets) {
+  template.name = `@jujulego/ring-${platform}-${arch}`;
+  template.description = `Ring ${platform} ${humanizedArchLookup[arch]} binary.`;
+  template.os = [nodeOsLookup[platform]];
+  template.cpu = [nodeArchLookup[arch]];
+
+  const outputPath = path.resolve(__dirname, '..', 'npm', `ring-${platform}-${arch}`);
+  fs.rmSync(outputPath, { recursive: true, force: true });
+  fs.mkdirSync(path.join(outputPath, 'bin'), { recursive: true });
+
+  if (platform === 'windows') {
+    fs.copyFileSync(
+        path.join(__dirname, 'templates', 'bin', 'ring'),
+        path.join(outputPath, 'bin', 'ring')
+    );
+  }
+
+  fs.writeFileSync(
+      path.join(outputPath, 'package.json'),
+      JSON.stringify(template, null, 2)
+  );
+}
