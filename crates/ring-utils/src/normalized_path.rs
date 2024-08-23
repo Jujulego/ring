2189bd89fc14bbb96a1dd30238ea7eda926960e2
Julien Capellari <julien.capellari@gmail.com>
2024-08-23 15:23:03 +0200

    feat: make NormalizedPathBuf mutable

--- crates/ring-utils/src/normalized_path.rs
+++ crates/ring-utils/src/normalized_path.rs
@@ -99,8 +99,46 @@ impl NormalizedPathBuf {
     /// assert_eq!(components.next(), Some(NormalizedComponent::Normal(OsStr::new("foo"))));
     /// assert_eq!(components.next(), None);
     /// ```
+    #[inline]
     pub fn components(&self) -> NormalizedComponents {
         NormalizedComponents { inner: self.inner.components() }
+    }
+
+    #[inline]
+    pub fn pop(&mut self) {
+        self.inner.pop();
+    }
+
+    /// Extends `self` with `path`.
+    ///
+    /// If `path` is absolute, it replaces the current path.
+    #[inline]
+    pub fn push<P: AsRef<Path>>(&mut self, path: P) {
+        self._push(path.as_ref())
+    }
+
+    fn _push(&mut self, path: &Path) {
+        if path.is_absolute() {
+            self.inner.clear();
+        }
+
+        path.components().for_each(|c| normalized_push(&mut self.inner, c));
+    }
+}
+
+fn normalized_push(path: &mut PathBuf, component: Component) {
+    match component {
+        Component::Prefix(..) => unreachable!(),
+        Component::RootDir => {
+            path.push(component.as_os_str());
+        }
+        Component::CurDir => {}
+        Component::ParentDir => {
+            path.pop();
+        }
+        Component::Normal(c) => {
+            path.push(c);
+        }
     }
 }
 
@@ -113,6 +151,17 @@ impl AsRef<OsStr> for NormalizedPathBuf {
 impl AsRef<Path> for NormalizedPathBuf {
     fn as_ref(&self) -> &Path {
         self.inner.as_ref()
+    }
+}
+
+impl<P: AsRef<Path>> Extend<P> for NormalizedPathBuf {
+    fn extend<I: IntoIterator<Item = P>>(&mut self, iter: I) {
+        iter.into_iter().for_each(move |p| self._push(p.as_ref()));
+    }
+
+    #[inline]
+    fn extend_one(&mut self, item: P) {
+        self._push(item.as_ref());
     }
 }
 
@@ -129,23 +178,17 @@ impl<T: ?Sized + AsRef<Path>> From<&T> for NormalizedPathBuf {
             PathBuf::new()
         };
 
-        for component in components {
-            match component {
-                Component::Prefix(..) => unreachable!(),
-                Component::RootDir => {
-                    inner.push(component.as_os_str());
-                }
-                Component::CurDir => {}
-                Component::ParentDir => {
-                    inner.pop();
-                }
-                Component::Normal(c) => {
-                    inner.push(c);
-                }
-            }
-        }
+        components.for_each(|c| normalized_push(&mut inner, c));
 
         NormalizedPathBuf { inner }
+    }
+}
+
+impl<P: AsRef<Path>> FromIterator<P> for NormalizedPathBuf {
+    fn from_iter<T: IntoIterator<Item=P>>(iter: T) -> Self {
+        let mut buf = NormalizedPathBuf::new();
+        buf.extend(iter);
+        buf
     }
 }
 
