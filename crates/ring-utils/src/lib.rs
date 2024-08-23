26a3f8eee72a6345e8b013a2b7a715db21b9af38
Julien Capellari <julien.capellari@gmail.com>
2024-08-23 15:02:40 +0200

    feat: add NormalizedPathBuf

--- crates/ring-utils/src/lib.rs
+++ crates/ring-utils/src/lib.rs
@@ -1,9 +1,11 @@
 mod macros;
+mod normalized_path;
 mod optional_result;
 mod path_tree;
 mod path;
 mod tag;
 
+pub use normalized_path::{NormalizedComponent, NormalizedPathBuf};
 pub use optional_result::OptionalResult;
 pub use path_tree::PathTree;
 pub use tag::Tag;
---
+++ crates/ring-utils/src/normalized_path.rs
@@ -0,0 +1,156 @@
+use std::ffi::OsStr;
+use std::fmt::Debug;
+use std::path::{Component, Components, Path, PathBuf, PrefixComponent};
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
+pub enum NormalizedComponent<'a> {
+    Prefix(PrefixComponent<'a>),
+    RootDir,
+    Normal(&'a OsStr),
+}
+
+impl<'a> From<Component<'a>> for NormalizedComponent<'a> {
+    fn from(value: Component<'a>) -> Self {
+        match value {
+            Component::Prefix(prefix) => NormalizedComponent::Prefix(prefix),
+            Component::RootDir => NormalizedComponent::RootDir,
+            Component::Normal(c) => NormalizedComponent::Normal(c),
+            c => panic!("un-normalized component {:?}", c),
+        }
+    }
+}
+
+impl<'a, 'b> PartialEq<Component<'a>> for NormalizedComponent<'b> {
+    fn eq(&self, other: &Component<'a>) -> bool {
+        match (self, other) {
+            (NormalizedComponent::Prefix(a), Component::Prefix(b)) => a == b,
+            (NormalizedComponent::RootDir, Component::RootDir) => true,
+            (NormalizedComponent::Normal(a), Component::Normal(b)) => a == b,
+            (_, _) => false,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct NormalizedComponents<'a> {
+    inner: Components<'a>,
+}
+
+impl<'a> Iterator for NormalizedComponents<'a> {
+    type Item = NormalizedComponent<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(NormalizedComponent::from)
+    }
+}
+
+impl<'a> DoubleEndedIterator for NormalizedComponents<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.inner.next_back().map(NormalizedComponent::from)
+    }
+}
+
+/// Wrapper of [`PathBuf`] representing path without any '.' or '..' components
+///
+/// # Examples
+///
+/// ```
+/// use ring_utils::NormalizedPathBuf;
+/// use std::path::Path;
+///
+/// let path = NormalizedPathBuf::from("/example/bar/baz/../foo");
+///
+/// assert_eq!(path, Path::new("/example/bar/foo"));
+/// ```
+#[derive(Clone, Default, Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct NormalizedPathBuf {
+    inner: PathBuf,
+}
+
+impl NormalizedPathBuf {
+    /// Allocates a new NormalizedPathBuf
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ring_utils::NormalizedPathBuf;
+    ///
+    /// let path = NormalizedPathBuf::new();
+    /// ```
+    #[inline]
+    pub fn new() -> NormalizedPathBuf {
+        NormalizedPathBuf { inner: PathBuf::new() }
+    }
+
+    /// Returns an iterator over [`NormalizedComponents`] of path
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::ffi::OsStr;
+    /// use ring_utils::{NormalizedComponent, NormalizedPathBuf};
+    ///
+    /// let path = NormalizedPathBuf::from("/example/bar/baz/../foo");
+    /// let mut components = path.components();
+    ///
+    /// assert_eq!(components.next(), Some(NormalizedComponent::RootDir));
+    /// assert_eq!(components.next(), Some(NormalizedComponent::Normal(OsStr::new("example"))));
+    /// assert_eq!(components.next(), Some(NormalizedComponent::Normal(OsStr::new("bar"))));
+    /// assert_eq!(components.next(), Some(NormalizedComponent::Normal(OsStr::new("foo"))));
+    /// assert_eq!(components.next(), None);
+    /// ```
+    pub fn components(&self) -> NormalizedComponents {
+        NormalizedComponents { inner: self.inner.components() }
+    }
+}
+
+impl AsRef<OsStr> for NormalizedPathBuf {
+    fn as_ref(&self) -> &OsStr {
+        self.inner.as_ref()
+    }
+}
+
+impl AsRef<Path> for NormalizedPathBuf {
+    fn as_ref(&self) -> &Path {
+        self.inner.as_ref()
+    }
+}
+
+impl<T: ?Sized + AsRef<Path>> From<&T> for NormalizedPathBuf {
+    /// Converts a borrowed [`Path`] to a [`NormalizedPathBuf`].
+    ///
+    /// Allocates a [`NormalizedPathBuf`] and copies the data into it.
+    fn from(value: &T) -> Self {
+        let mut components = value.as_ref().components().peekable();
+        let mut inner = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
+            components.next();
+            PathBuf::from(c.as_os_str())
+        } else {
+            PathBuf::new()
+        };
+
+        for component in components {
+            match component {
+                Component::Prefix(..) => unreachable!(),
+                Component::RootDir => {
+                    inner.push(component.as_os_str());
+                }
+                Component::CurDir => {}
+                Component::ParentDir => {
+                    inner.pop();
+                }
+                Component::Normal(c) => {
+                    inner.push(c);
+                }
+            }
+        }
+
+        NormalizedPathBuf { inner }
+    }
+}
+
+impl PartialEq<&Path> for NormalizedPathBuf {
+    fn eq(&self, other: &&Path) -> bool {
+        self.components().eq(other.components())
+    }
+}
