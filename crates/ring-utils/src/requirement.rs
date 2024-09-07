a59f43bbd3dad5f37804ad873d6cc042870160c9
Julien Capellari <julien.capellari@gmail.com>
2024-09-07 23:16:24 +0200

    feat: impl display for requirement

--- crates/ring-utils/src/requirement.rs
+++ crates/ring-utils/src/requirement.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use semver::VersionReq;
 use crate::NormalizedPathBuf;
 
@@ -6,4 +7,28 @@ pub enum Requirement {
     Any,
     Path(NormalizedPathBuf),
     Version(VersionReq),
+}
+
+impl Display for Requirement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Requirement::Any => write!(f, "any"),
+            Requirement::Path(path) => write!(f, "path:{}", path.display()),
+            Requirement::Version(version) => write!(f, "version:{}", version),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use crate::Normalize;
+    use super::*;
+    
+    #[test]
+    fn it_should_display_requirement() {
+        assert_eq!(format!("{}", Requirement::Any), "any");
+        assert_eq!(format!("{}", Requirement::Path(Path::new("/test").normalize())), String::from("path:") + std::path::MAIN_SEPARATOR_STR + "test");
+        assert_eq!(format!("{}", Requirement::Version(VersionReq::parse("^1.0.0").unwrap())), "version:^1.0.0");
+    }
 }
