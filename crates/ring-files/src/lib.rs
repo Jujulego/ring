8f106d41c2baef63efbdde52fbb73733b316e702
Julien Capellari <julien.capellari@gmail.com>
2024-09-17 23:11:58 +0200

    chore: global reset

--- crates/ring-files/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod manifest_loader;
-mod pattern_iterator;
-
-pub use manifest_loader::ManifestLoader;
-pub use pattern_iterator::{PatternIterator, ResolvedPatterns};
--- crates/ring-utils/src/macros.rs
+++
@@ -1,6 +0,0 @@
-#[macro_export]
-macro_rules! absolute_path {
-    ($path:literal) => {
-        std::path::PathBuf::from((if cfg!(windows) { r"C:\" } else { "/" }).to_owned() + $path)
-    }
-}
