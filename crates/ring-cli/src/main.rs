6a54675bbbf6d0e270e9d82147a91c8e2799e539
Julien Capellari <julien.capellari@gmail.com>
2024-09-17 23:11:58 +0200

    chore: global reset

--- crates/ring-cli/src/main.rs
+++ crates/ring-cli/src/main.rs
@@ -1,25 +1,13 @@
 use std::io;
-use anyhow::Result;
 use clap::{arg, ArgAction, Command};
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
-use ring_core::RingCore;
 
-mod list;
-mod modules;
-mod projects;
-
-fn main() -> Result<()> {
+fn main() -> anyhow::Result<()> {
     // Setup commands
     let args = Command::new("ring")
         .version(env!("RING_CLI_VERSION"))
         .propagate_version(true)
-        .subcommand_required(true)
-        .subcommands([
-            list::build_command(),
-            modules::build_command(),
-            projects::build_command()
-        ])
         .arg(arg!(-v --verbose)
             .global(true)
             .required(false)
@@ -40,14 +28,6 @@ fn main() -> Result<()> {
         .finish();
 
     tracing::subscriber::set_global_default(subscriber)?;
-
-    // Handle subcommands
-    let core = RingCore::new();
     
-    match args.subcommand() {
-        Some(("list", args)) => list::handle_command(&core, args),
-        Some(("modules", args)) => modules::handle_command(&core, args),
-        Some(("projects", args)) => projects::handle_command(&core, args),
-        _ => unreachable!()
-    }
+    Ok(())
 }
