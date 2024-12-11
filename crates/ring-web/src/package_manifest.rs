206f1ca50f9a08e4fab76bd664065cca8465f728
Julien Capellari <julien.capellari@gmail.com>
2024-12-11 23:47:12 +0100

    fix: handle nameless web package

--- crates/ring-web/src/package_manifest.rs
+++ crates/ring-web/src/package_manifest.rs
@@ -5,7 +5,7 @@ use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct PackageManifest {
-    pub name: String,
+    pub name: Option<String>,
     #[serde(default)]
     pub version: Option<Version>,
     #[serde(default)]
