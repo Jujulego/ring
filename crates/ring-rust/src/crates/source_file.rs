13926046b77fa01670857bbfd80ae1391a9c7faf
Julien Capellari <julien.capellari@gmail.com>
2024-12-27 00:27:41 +0100

    feat: tag rust source file

--- crates/ring-rust/src/crates/source_file.rs
+++ crates/ring-rust/src/crates/source_file.rs
@@ -1,6 +1,8 @@
-use crate::language::rust_language;
+use crate::language::{rust_language, RUST_COLOR};
+use ring_color::ColorLabel;
 use ring_core::{CodeLanguage, CodeUnit};
-use ring_tag::Tagged;
+use ring_tag::{Tag, Tagged};
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
 /// Represents a rust source file
@@ -30,4 +32,20 @@ impl CodeUnit for SourceFile {
     }
 }
 
-impl Tagged for SourceFile {}
+impl Tagged for SourceFile {
+    fn tags(&self) -> Vec<Tag> {
+        let mut tags = vec![];
+
+        let dirname = self.path.parent().and_then(Path::file_name).and_then(OsStr::to_str);
+        let filename = self.path.file_name().and_then(OsStr::to_str);
+
+        match (dirname, filename) {
+            (Some("src"), Some("lib.rs")) |
+            (Some("src"), Some("main.rs")) => tags.push(Tag::from("rust:main").with_color(ColorLabel::Red, RUST_COLOR)),
+            (Some(dir), Some("build.rs")) if dir != "src" => tags.push(Tag::from("rust:build").with_color(ColorLabel::Red, RUST_COLOR)),
+            _ => {}
+        }
+
+        tags
+    }
+}
--- crates/ring-rust/src/language.rs
+++ crates/ring-rust/src/language.rs
@@ -2,9 +2,11 @@ use rgb::Rgb;
 use ring_color::{ColorLabel, StableColor};
 use ring_core::CodeLanguage;
 
+pub const RUST_COLOR: Rgb<u8> = Rgb { r: 0xe3, g: 0x3b, b: 0x26 };
+
 pub fn rust_language() -> CodeLanguage {
     CodeLanguage::new(
         "rust".to_string(),
-        StableColor::new(ColorLabel::BrightRed, Rgb { r: 0xe3, g: 0x3b, b: 0x26 }),
+        StableColor::new(ColorLabel::Red, RUST_COLOR),
     )
 }
