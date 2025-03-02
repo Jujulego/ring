bbb824cac522ca01e7d601d56977c423a9e21d0e
Julien Capellari <julien.capellari@gmail.com>
2025-03-02 22:33:21 +0100

    feat: add some doc tests

--- crates/ring-core/src/cache/path_cache.rs
+++ crates/ring-core/src/cache/path_cache.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::io;
 use std::path::{Path, PathBuf};
 
-/// Cache based on path keys.
+/// Cache based on absolute path keys.
 #[derive(Clone, Debug)]
 pub struct PathCache<V> {
     cache: HashMap<PathBuf, V>,
@@ -10,6 +10,14 @@ pub struct PathCache<V> {
 
 impl<V> PathCache<V> {
     /// Creates a new empty path cache
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ring_core::cache::PathCache;
+    ///
+    /// let mut cache: PathCache<String> = PathCache::new();
+    /// ```
     #[inline]
     pub fn new() -> Self {
         Default::default()
@@ -19,6 +27,18 @@ impl<V> PathCache<V> {
     ///
     /// Given path is resolved using [`std::path::absolute`] before searching in the cache.
     /// Returns an error if the path resolution fails.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ring_core::cache::PathCache;
+    ///
+    /// let mut cache: PathCache<String> = PathCache::new();
+    /// assert_eq!(cache.get("/toto").unwrap(), None);
+    ///
+    /// cache.insert("/toto", "toto".to_string()).unwrap();
+    /// assert_eq!(cache.get("/toto").unwrap(), Some(&"toto".to_string()));
+    /// ```
     pub fn get<P: AsRef<Path>>(&mut self, path: P) -> io::Result<Option<&V>> {
         Ok(self.cache.get(&std::path::absolute(path)?))
     }
@@ -27,6 +47,15 @@ impl<V> PathCache<V> {
     ///
     /// Given path is resolved using [`std::path::absolute`] before searching in the cache.
     /// Returns an error if the path resolution fails.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ring_core::cache::PathCache;
+    ///
+    /// let mut cache: PathCache<String> = PathCache::new();
+    /// assert!(cache.insert("/toto", "toto".to_string()).is_ok());
+    /// ```
     pub fn insert<P: AsRef<Path>>(&mut self, path: P, value: V) -> io::Result<Option<V>> {
         Ok(self.cache.insert(std::path::absolute(path)?, value))
     }
@@ -40,6 +69,7 @@ impl<V> PathCache<V> {
     ///
     /// let mut cache: PathCache<String> = PathCache::new();
     /// assert!(cache.is_empty());
+    ///
     /// cache.insert("/toto", "toto".to_string()).unwrap();
     /// assert!(!cache.is_empty());
     /// ```
@@ -57,6 +87,7 @@ impl<V> PathCache<V> {
     ///
     /// let mut cache: PathCache<String> = PathCache::new();
     /// assert_eq!(cache.len(), 0);
+    ///
     /// cache.insert("/toto", "toto".to_string()).unwrap();
     /// assert_eq!(cache.len(), 1);
     /// ```
