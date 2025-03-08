a3d69482545d15624d9e74544a27a6e02a5a8c07
Julien Capellari <julien.capellari@gmail.com>
2025-03-08 11:56:52 +0100

    Add CachedIdentifier (#371)
    * feat: implement cached identifier
    
    * feat: test cached identifier
    
    * feat: test cached identifier

--- crates/ring-core/src/cache/mod.rs
+++ crates/ring-core/src/cache/mod.rs
@@ -1,3 +1,5 @@
+mod cached_identifier;
 mod path_cache;
 
+pub use cached_identifier::CachedIdentifier;
 pub use path_cache::PathCache;
--- crates/ring-core/src/units/identifier.rs
+++ crates/ring-core/src/units/identifier.rs
@@ -1,10 +1,12 @@
 use crate::units::Unit;
+use std::borrow::Borrow;
 use std::path::Path;
 
 /// Object able to identify code units
 pub trait Identifier {
     type Unit: Unit;
+    type Output: Borrow<Self::Unit>;
 
     /// Identify unit at given path
-    fn identify_unit(&self, path: &Path) -> anyhow::Result<Option<Self::Unit>>;
+    fn identify_unit(&self, path: &Path) -> anyhow::Result<Option<Self::Output>>;
 }
