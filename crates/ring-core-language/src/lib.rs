a057fb579197a7ffcc3ed32976d00c347427ad7b
Julien Capellari <julien.capellari@gmail.com>
2025-03-12 10:48:49 +0100

    feat: define language structure

--- crates/ring-core-language/src/lib.rs
+++ crates/ring-core-language/src/lib.rs
@@ -1,14 +1,73 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use rgb::Rgb;
+
+/// Represents a language
+#[derive(Clone, Debug)]
+pub struct Language {
+    name: String,
+    color: Option<Rgb<u8>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Language {
+    /// Creates a new language
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// use ring_core_language::Language;
+    /// 
+    /// let language = Language::new("example".to_string());
+    /// ```
+    #[inline]
+    pub fn new(name: String) -> Language {
+        Language { name, color: None }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Adds a color to the language
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ring_core_language::Language;
+    ///
+    /// let language = Language::new("example".to_string()).with_color((0x00, 0xff, 0x00).into());
+    /// ```
+    #[inline]
+    pub fn with_color(mut self, color: Rgb<u8>) -> Language {
+        self.color = Some(color);
+        self
+    }
+    
+    /// Returns language's name
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use ring_core_language::Language;
+    ///
+    /// let language = Language::new("example".to_string());
+    /// assert_eq!(language.name(), "example");
+    /// ```
+    #[inline]
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Returns language's color, if any
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use rgb::Rgb;
+    /// use ring_core_language::Language;
+    ///
+    /// let language = Language::new("example".to_string());
+    /// assert_eq!(language.color(), None);
+    ///
+    /// let language = Language::new("example".to_string()).with_color((0x00, 0xff, 0x00).into());
+    /// assert_eq!(language.color(), Some(Rgb::new(0x00, 0xff, 0x00)));
+    /// ```
+    #[inline]
+    pub fn color(&self) -> Option<&Rgb<u8>> {
+        self.color.as_ref()
     }
 }
