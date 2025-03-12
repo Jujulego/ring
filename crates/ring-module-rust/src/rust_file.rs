a1efd0a32e3bcb60a953d0828d4ba12949529ffe
Julien Capellari <julien.capellari@gmail.com>
2025-03-12 23:40:19 +0100

    feat: split rust file detector logic

--- crates/ring-module-rust/src/rust_file.rs
+++ crates/ring-module-rust/src/rust_file.rs
@@ -8,19 +8,29 @@ use crate::rust_language;
 pub struct RustFileDetector {}
 
 impl RustFileDetector {
+    #[inline]
     pub fn new() -> Self {
-        RustFileDetector {}
+        Default::default()
+    }
+    
+    #[inline]
+    pub fn is_rust_file<P: AsRef<Path>>(&self, path: P) -> bool {
+        self._is_rust_file(path.as_ref())
+    }
+    
+    fn _is_rust_file(&self, path: &Path) -> bool {
+        trace!("touch path {}", path.display());
+        path.is_file() && path.extension()
+            .and_then(OsStr::to_str)
+            .map_or(false, |ext| ext == "rs")
     }
 }
 
 impl DetectLanguage for RustFileDetector {
     #[instrument(name = "rust-file.detect-language", skip_all)]
     fn detect_language(&self, path: &Path) -> Option<Language> {
-        trace!("touch path {}", path.display());
-        if path.is_file() {
-            path.extension()
-                .and_then(OsStr::to_str)
-                .and_then(|ext| if ext == "rs" { Some(rust_language()) } else { None })
+        if self._is_rust_file(path) {
+            Some(rust_language())
         } else {
             None
         }
