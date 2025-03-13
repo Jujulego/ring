686a415f731d10ecc4ad684589a61587f0af44e6
Julien Capellari <julien.capellari@gmail.com>
2025-03-13 09:37:53 +0100

    feat: document core

--- crates/ring-cli/src/commands/list.rs
+++ crates/ring-cli/src/commands/list.rs
@@ -1,12 +1,12 @@
-use crossterm::style::{style, Color, Stylize};
 use clap::{arg, value_parser, ArgAction, ArgMatches, Command};
+use crossterm::style::{style, Color, Stylize};
 use ring_cli_list::FilesIterator;
+use ring_core::Core;
+use ring_core_language::Language;
 use std::io::IsTerminal;
 use std::path::PathBuf;
 use std::{env, io};
 use tracing::instrument;
-use ring_core::{detect_language, Core};
-use ring_core_language::Language;
 
 /// Prepare list command parsing
 pub fn setup() -> Command {
@@ -39,7 +39,7 @@ pub fn handle(core: &Core, args: &ArgMatches) -> anyhow::Result<()> {
     for file in files {
         let file = file?;
         let file_name = file.file_name().unwrap_or_default();
-        let language = detect_language(core, file.path());
+        let language = core.detect_language(file.path());
         
         if io::stdout().is_terminal() {
             let file_name = ls_colors.style_for(&file)
--- crates/ring-core/src/lib.rs
+++ crates/ring-core/src/lib.rs
@@ -8,6 +8,15 @@ pub struct Core {
 }
 
 impl Core {
+    /// Initiate all modules
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ring_core::Core;
+    /// 
+    /// let core = Core::new();
+    /// ```
     pub fn new() -> Self {
         let mut modules: Vec<Box<dyn Module>> = Vec::new();
 
@@ -16,16 +25,28 @@ impl Core {
 
         Core { modules }
     }
-}
 
-#[inline]
-pub fn detect_language<P: AsRef<Path>>(core: &Core, path: P) -> Option<Language> {
-    _detect_language(core, path.as_ref())
-}
+    /// Uses all modules to detect given path's language
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use ring_core::Core;
+    /// use ring_module_rust::rust_language;
+    ///
+    /// let core = Core::new();
+    /// assert_eq!(core.detect_language("src"), None);
+    /// assert_eq!(core.detect_language("src/lib.rs"), Some(rust_language()));
+    /// ```
+    #[inline]
+    pub fn detect_language<P: AsRef<Path>>(&self, path: P) -> Option<Language> {
+        self._detect_language(path.as_ref())
+    }
 
-fn _detect_language(core: &Core, path: &Path) -> Option<Language> {
-    core.modules.iter()
-        .flat_map(|module| module.language_detectors())
-        .filter_map(|detector| detector.detect_language(path))
-        .next()
+    fn _detect_language(&self, path: &Path) -> Option<Language> {
+        self.modules.iter()
+            .flat_map(|module| module.language_detectors())
+            .filter_map(|detector| detector.detect_language(path))
+            .next()
+    }
 }
