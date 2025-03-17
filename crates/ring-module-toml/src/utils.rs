337a57187b6ed7a47933a763826db7ec0f867c53
Julien Capellari <julien.capellari@gmail.com>
2025-03-17 18:23:30 +0100

    Detect toml language (#386)
    * feat: add new toml module
    
    * feat: load toml module
    
    * fix: tests
    
    * chore: run doc tests with all features

---
+++ crates/ring-module-toml/src/utils.rs
@@ -0,0 +1,17 @@
+use ring_core_language::Language;
+
+/// Creates a rust language object, with a rusty color
+///
+/// # Example
+///
+/// ```
+/// use ring_module_toml::toml_language;
+///
+/// let language = toml_language();
+/// assert_eq!(language.name(), "toml");
+/// assert_eq!(language.color(), Some(&(0x9c, 0x42, 0x21).into()));
+/// ```
+#[inline]
+pub fn toml_language() -> Language {
+    Language::new("toml".to_string()).with_color((0x9c, 0x42, 0x21).into())
+}
