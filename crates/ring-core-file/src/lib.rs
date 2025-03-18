8c421d8be37852ca4e6dcf0f6b876aae86705a1e
Julien Capellari <julien.capellari@gmail.com>
2025-03-18 19:00:48 +0100

    feat: define QualifyFile trait

---
+++ crates/ring-core-file/src/lib.rs
@@ -0,0 +1,15 @@
+use std::path::Path;
+
+/// Object able to qualify a given path
+pub trait QualifyPath {
+    fn qualify_content(&self, path: &Path) -> FileContent;
+}
+
+/// Define the kind of content detected inside a file
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum FileContent {
+    Manifest,
+    Source,
+    Test,
+    Other(String),
+}
