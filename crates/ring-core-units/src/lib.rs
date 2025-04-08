61bd804ae830a83a38507ed2d521fa26370a8990
Julien Capellari <julien.capellari@gmail.com>
2025-04-08 23:21:34 +0200

    feat: define detect unit trait

--- crates/ring-core-units/src/lib.rs
+++ crates/ring-core-units/src/lib.rs
@@ -1,3 +1,10 @@
 mod unit;
 
+use std::path::Path;
+use std::rc::Rc;
 pub use unit::Unit;
+
+/// Object able to detect a unit at given path
+pub trait DetectUnit {
+    fn detect_unit(&self, path: &Path) -> Rc<dyn Unit>;
+}
