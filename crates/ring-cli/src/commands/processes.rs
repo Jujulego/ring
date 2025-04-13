0690d269fb252197526091967fb1321afa82662d
Julien Capellari <julien.capellari@gmail.com>
2025-04-13 23:36:01 +0200

    feat: basic implementation

--- crates/ring-cli/src/commands/processes.rs
+++ crates/ring-cli/src/commands/processes.rs
@@ -1,5 +1,7 @@
 use clap::Command;
-use tracing::instrument;
+use sysinfo::{ProcessRefreshKind, RefreshKind, System};
+use tracing::{instrument, trace};
+use ring_cli_list::List;
 
 /// Prepare processes command parsing
 pub fn setup() -> Command {
@@ -11,5 +13,24 @@ pub fn setup() -> Command {
 /// Handle processes command execution
 #[instrument(name = "cli.processes", skip_all)]
 pub fn handle() -> anyhow::Result<()> {
+    // List processes
+    trace!("load running processes");
+    let sys = System::new_with_specifics(
+        RefreshKind::nothing()
+            .with_processes(ProcessRefreshKind::everything()),
+    );
+
+    let list = sys.processes().iter()
+        .map(|(pid, process)| vec![
+            pid.to_string(),
+            process.cmd().first()
+                .and_then(|c| c.to_str())
+                .unwrap_or("unknown")
+                .to_string()
+        ])
+        .collect::<List>();
+
+    print!("{}", list);
+
     Ok(())
 }
