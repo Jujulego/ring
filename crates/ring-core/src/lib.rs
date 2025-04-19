bd187c62370c46e0d6a4d506a563be78a1d5d57c
Julien Capellari <julien.capellari@gmail.com>
2025-04-19 11:25:29 +0200

    feat: add detect_task call on core

--- crates/ring-core/src/lib.rs
+++ crates/ring-core/src/lib.rs
@@ -3,6 +3,8 @@ use ring_core_modules::Module;
 use ring_core_units::Unit;
 use std::path::{absolute, Path};
 use std::rc::Rc;
+use sysinfo::Process;
+use ring_core_tasks::Task;
 
 /// Holds and manages all modules references
 pub struct Core {
@@ -59,6 +61,14 @@ impl Core {
             .flat_map(|module| module.language_detectors())
             .filter_map(|detector| detector.detect_language(path))
             .next()
+    }
+
+    /// Uses all modules to detect task of given process
+    pub fn detect_tasks(&self, process: &Process) -> Vec<Rc<dyn Task>> {
+        self.modules.iter()
+            .flat_map(|module| module.task_detectors())
+            .filter_map(|detector| detector.detect_task(process))
+            .collect()
     }
 
     /// Uses all modules to detect units at given path
