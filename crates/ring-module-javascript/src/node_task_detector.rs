6c4d03875d7c684fd61b20d32e29a65a6d10d4c3
Julien Capellari <julien.capellari@gmail.com>
2025-04-20 23:07:07 +0200

    feat: parse node command line

--- crates/ring-module-javascript/src/node_task_detector.rs
+++ crates/ring-module-javascript/src/node_task_detector.rs
@@ -1,11 +1,11 @@
-use std::ffi::OsStr;
+use crate::node_task::NodeTask;
+use crate::NpmPackageDetector;
+use ring_core_tasks::{DetectTask, Task};
+use std::ffi::{OsStr, OsString};
 use std::path::PathBuf;
 use std::rc::Rc;
 use sysinfo::Process;
 use tracing::{debug, instrument, warn};
-use ring_core_tasks::{DetectTask, Task};
-use crate::node_task::NodeTask;
-use crate::NpmPackageDetector;
 
 #[derive(Clone, Debug)]
 pub struct NodeTaskDetector {
@@ -29,18 +29,18 @@ impl NodeTaskDetector {
     /// Builds a `NodeTask` object from given process.
     pub fn load_node_task(&self, process: &Process) -> anyhow::Result<Option<Rc<NodeTask>>> {
         if self.is_node_task(process) {
+            let mut script = extract_node_script(&process.cmd()[1..])
+                .map(PathBuf::from)
+                .unwrap_or_else(|| process.exe().unwrap().to_path_buf());
+
             if let Some(cwd) = process.cwd() {
-                let script = process.cmd().get(1)
-                    .map(PathBuf::from)
-                    .unwrap_or_else(|| process.exe().unwrap().to_path_buf());
-
-                let script = cwd.join(script);
-                let package = self.npm_package_detector.load_package_containing(&script)?;
-
-                let task = NodeTask::new(script, package);
-
-                return Ok(Some(Rc::new(task)))
+                script = cwd.join(script);
             }
+
+            let package = self.npm_package_detector.load_package_containing(&script)?;
+            let task = NodeTask::new(script, package);
+
+            return Ok(Some(Rc::new(task)))
         }
 
         Ok(None)
@@ -62,4 +62,23 @@ impl DetectTask for NodeTaskDetector {
             }
         }
     }
+}
+
+fn extract_node_script(mut args: &[OsString]) -> Option<&OsStr> {
+    while !args.is_empty() {
+        let arg = args.first().unwrap().to_str().unwrap();
+
+        if arg.starts_with('-') {
+            if ["-r", "--require", "--import"].contains(&arg) {
+                args = &args[2..];
+            } else {
+                args = &args[1..];
+            }
+        } else {
+            break;
+        }
+    }
+
+    args.first()
+        .map(|str| str.as_os_str())
 }
