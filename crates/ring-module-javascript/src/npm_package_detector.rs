e7224908819db6e13f6da4cded65652cffe510eb
Julien Capellari <julien.capellari@gmail.com>
2025-04-24 01:20:00 +0200

    feat: improve js files qualification

--- crates/ring-module-javascript/src/npm_package_detector.rs
+++ crates/ring-module-javascript/src/npm_package_detector.rs
@@ -68,6 +68,10 @@ impl NpmPackageDetector {
         path.is_file() && (self._is_npm_lockfile(path) || self._is_pnpm_lockfile(path) || self._is_yarn_lockfile(path))
     }
 
+    fn _is_npm_configuration(&self, path: &Path) -> bool {
+        path.file_name().and_then(OsStr::to_str) == Some(".npmrc")
+    }
+
     /// Checks if given path is a npm package lockfile
     ///
     /// # Examples
@@ -170,7 +174,7 @@ impl NpmPackageDetector {
     }
 
     fn _is_yarn_configuration(&self, path: &Path) -> bool {
-        path.file_name().and_then(OsStr::to_str) == Some(".yarnrc.yml")
+        matches!(path.file_name().and_then(OsStr::to_str), Some(".yarnrc") | Some(".yarnrc.yml"))
     }
 
     /// Load npm package at given path, if any.
@@ -302,6 +306,20 @@ impl QualifyPath for NpmPackageDetector {
             if self._is_manifest(path) {
                 return Some((FileContent::Manifest, path));
             }
+
+            if self._is_npm_configuration(path) || self._is_yarn_configuration(path) {
+                return Some((FileContent::Configuration, path));
+            }
+
+            if self._is_npm_lockfile(path) || self._is_pnpm_lockfile(path) || self._is_yarn_lockfile(path) {
+                return Some((FileContent::Lockfile, path));
+            }
+
+            match path.file_name().and_then(OsStr::to_str) {
+                Some(".pnp.cjs") => return Some((FileContent::Other("pnp-commonjs".into()), path)),
+                Some(".pnp.loader.mjs") => return Some((FileContent::Other("pnp-esm".into()), path)),
+                _ => {}
+            }
         } else {
             return None;
         }
@@ -314,23 +332,6 @@ impl QualifyPath for NpmPackageDetector {
                 }
             } else {
                 break;
-            }
-
-            trace!("stat {}", ancestor.display());
-            if ancestor.is_file() {
-                if self._is_yarn_configuration(ancestor) {
-                    return Some((FileContent::Configuration, ancestor));
-                }
-
-                if self._is_npm_lockfile(ancestor) || self._is_pnpm_lockfile(ancestor) || self._is_yarn_lockfile(ancestor) {
-                    return Some((FileContent::Lockfile, ancestor));
-                }
-
-                match ancestor.file_name().and_then(OsStr::to_str) {
-                    Some(".pnp.cjs") => return Some((FileContent::Other("pnp commonjs".into()), ancestor)),
-                    Some(".pnp.loader.mjs") => return Some((FileContent::Other("pnp esm".into()), ancestor)),
-                    _ => {}
-                }
             }
         }
 
