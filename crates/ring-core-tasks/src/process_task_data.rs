71b65fd60445746df2b35588e90869651970bc93
Julien Capellari <julien.capellari@gmail.com>
2025-05-18 14:08:55 +0200

    test: process task data

--- crates/ring-core-tasks/src/process_task_data.rs
+++ crates/ring-core-tasks/src/process_task_data.rs
@@ -67,3 +67,46 @@ impl ProcessTask for ProcessTaskData {
         self.clone()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_should_implement_task_trait() {
+        let data = ProcessTaskData {
+            id: "id".to_string(),
+            kind: "kind".to_string(),
+            args: vec![],
+            color: Some(Rgb { r: 0, g: 0, b: 0 }),
+            executable: PathBuf::from("/test/exe"),
+            script: Some(PathBuf::from("/test/script")),
+            working_directory: PathBuf::from("/test"),
+            working_unit: None,
+        };
+
+        assert_eq!(data.id(), "id");
+        assert_eq!(data.kind(), "kind");
+        assert!(data.working_unit().is_none());
+        assert_eq!(data.color(), Some(Rgb { r: 0, g: 0, b: 0 }));
+    }
+
+    #[test]
+    fn it_should_implement_process_task_trait() {
+        let data = ProcessTaskData {
+            id: "id".to_string(),
+            kind: "kind".to_string(),
+            args: vec![],
+            color: Some(Rgb { r: 0, g: 0, b: 0 }),
+            executable: PathBuf::from("/test/exe"),
+            script: Some(PathBuf::from("/test/script")),
+            working_directory: PathBuf::from("/test"),
+            working_unit: None,
+        };
+
+        assert_eq!(data.executable(), Path::new("/test/exe"));
+        assert_eq!(data.working_directory(), Path::new("/test"));
+        assert!(data.args().is_empty());
+        assert_eq!(data.script(), Some(Path::new("/test/script")));
+    }
+}
