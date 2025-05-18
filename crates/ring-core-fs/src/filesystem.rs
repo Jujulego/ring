399250c0870a036103c5102dbe2cd2979a998c63
Julien Capellari <julien.capellari@gmail.com>
2025-05-18 22:27:20 +0200

    feat: add support detection

--- crates/ring-core-fs/src/filesystem.rs
+++ crates/ring-core-fs/src/filesystem.rs
@@ -1,10 +1,16 @@
 use std::path::Path;
 use crate::PathAdaptator;
 
-/// Access files on the filesystem
+/// Access files on the filesystem. Supports any path.
 pub struct Filesystem;
 
 impl PathAdaptator for Filesystem {
+    #[inline]
+    fn is_supported(&self, _: &Path) -> bool {
+        true
+    }
+
+
     #[inline]
     fn is_file(&self, path: &Path) -> anyhow::Result<bool> {
         Ok(path.is_file())
@@ -14,9 +20,17 @@ impl PathAdaptator for Filesystem {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
-    fn is_file_should_return_true() {
+    fn is_supported_should_return_true() {
+        assert!(Filesystem.is_supported(Path::new("assets/foo.txt")));
+    }
+
+    #[test]
+    fn is_file_should_detect_files() {
         assert!(Filesystem.is_file(Path::new("assets/foo.txt")).unwrap());
+
+        assert!(!Filesystem.is_file(Path::new("assets")).unwrap());
+        assert!(!Filesystem.is_file(Path::new("assets/do-no-exists")).unwrap());
     }
 }
--- crates/ring-core-fs/src/path_adaptator.rs
+++ crates/ring-core-fs/src/path_adaptator.rs
@@ -2,6 +2,9 @@ use std::path::Path;
 
 /// Path based filesystem adaptator
 pub trait PathAdaptator {
+    /// Indicates if the adaptator can handle given path
+    fn is_supported(&self, path: &Path) -> bool;
+    
     /// Tests if given path exists and is a file
     fn is_file(&self, path: &Path) -> anyhow::Result<bool>;
 }
