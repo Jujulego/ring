6f4377a84fd2718597bafc5ea359721dde98814c
Julien Capellari <julien.capellari@gmail.com>
2025-05-18 15:20:07 +0200

    feat: add powershell script detection

--- crates/ring-module-shell/src/lib.rs
+++ crates/ring-module-shell/src/lib.rs
@@ -1,3 +1,4 @@
+mod powershell_file_detector;
 mod powershell_task;
 mod powershell_task_detector;
 mod shell_file_detector;
@@ -5,11 +6,12 @@ mod shell_task;
 mod shell_task_detector;
 mod utils;
 
-use crate::powershell_task_detector::PowershellTaskDetector;
+pub use crate::powershell_file_detector::PowershellFileDetector;
+pub use crate::powershell_task_detector::PowershellTaskDetector;
 pub use crate::shell_file_detector::ShellFileDetector;
 pub use crate::shell_task::{ShellKind, ShellTask};
 pub use crate::shell_task_detector::ShellTaskDetector;
-pub use crate::utils::shell_language;
+pub use crate::utils::{powershell_language, shell_language};
 use ring_core_content::{DetectLanguage, QualifyPath};
 use ring_core_modules::{Module, RegistryRef};
 use ring_core_tasks::DetectProcessTask;
@@ -17,6 +19,7 @@ use std::rc::Rc;
 
 #[derive(Clone)]
 pub struct ShellModule {
+    powershell_file_detector: Rc<PowershellFileDetector>,
     powershell_task_detector: Rc<PowershellTaskDetector>,
     shell_file_detector: Rc<ShellFileDetector>,
     shell_task_detector: Rc<ShellTaskDetector>,
@@ -27,10 +30,17 @@ impl ShellModule {
     #[inline]
     pub fn new(registry: Rc<RegistryRef>) -> Self {
         ShellModule {
+            powershell_file_detector: Rc::new(PowershellFileDetector::new()),
             powershell_task_detector: Rc::new(PowershellTaskDetector::new(registry.clone())),
             shell_file_detector: Rc::new(ShellFileDetector::new()),
             shell_task_detector: Rc::new(ShellTaskDetector::new(registry)),
         }
+    }
+
+    /// Returns a pointer on PowershellFileDetector
+    #[inline]
+    pub fn powershell_file_detector(&self) -> Rc<PowershellFileDetector> {
+        self.powershell_file_detector.clone()
     }
 
     /// Returns a pointer on PowershellTaskDetector
@@ -55,12 +65,12 @@ impl ShellModule {
 impl Module for ShellModule {
     #[inline]
     fn file_qualifiers(&self) -> Vec<Rc<dyn QualifyPath>> {
-        vec![self.shell_file_detector()]
+        vec![self.powershell_file_detector(), self.shell_file_detector()]
     }
 
     #[inline]
     fn language_detectors(&self) -> Vec<Rc<dyn DetectLanguage>> {
-        vec![self.shell_file_detector()]
+        vec![self.powershell_file_detector(), self.shell_file_detector()]
     }
 
     #[inline]
--- crates/ring-module-shell/src/utils.rs
+++ crates/ring-module-shell/src/utils.rs
@@ -1,5 +1,19 @@
 use ring_core_content::Language;
 
+/// Creates a powershell language object
+///
+/// # Example
+///
+/// ```
+/// use ring_module_shell::powershell_language;
+///
+/// assert_eq!(powershell_language().name(), "powershell");
+/// ```
+#[inline]
+pub fn powershell_language() -> Language {
+    Language::new("powershell".to_string()).with_color((0x42, 0x72, 0xc9).into())
+}
+
 /// Creates a shell language object
 ///
 /// # Example
@@ -9,9 +23,30 @@ use ring_core_content::Language;
 ///
 /// let language = shell_language();
 /// assert_eq!(language.name(), "shell");
-/// assert_eq!(language.color(), None);
 /// ```
 #[inline]
 pub fn shell_language() -> Language {
     Language::new("shell".to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use rgb::Rgb;
+    use super::*;
+
+    #[test]
+    fn is_should_build_a_powershell_language() {
+        let language = powershell_language();
+
+        assert_eq!(language.name(), "powershell");
+        assert_eq!(language.color(), Some(&Rgb { r: 0x42, g: 0x72, b: 0xc9 }));
+    }
+
+    #[test]
+    fn is_should_build_a_shell_language() {
+        let language = shell_language();
+
+        assert_eq!(language.name(), "shell");
+        assert_eq!(language.color(), None);
+    }
 }
