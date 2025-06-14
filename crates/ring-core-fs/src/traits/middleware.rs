b1403339a62e56df1aa630d6220cdc994221e24d
Julien Capellari <julien.capellari@gmail.com>
2025-06-15 00:28:47 +0200

    feat: implement read dir for zip middleware

--- crates/ring-core-fs/src/traits/middleware.rs
+++ crates/ring-core-fs/src/traits/middleware.rs
@@ -1,8 +1,11 @@
-use crate::traits::{Location, MaybeLocationMetadata};
+use crate::traits::{Location, LocationIterator, MaybeLocationMetadata};
 use crate::FsError;
 use std::path::Path;
 
 pub trait FilesystemMiddleware: MaybeLocationMetadata {
     /// Resolves given path and check if it points to anything
     fn maybe_locate_path(&self, path: &Path) -> Option<Result<Box<dyn Location + '_>, FsError>>;
+
+    /// Returns an iterator over path child locations
+    fn maybe_read_dir(&self, path: &Path) -> Option<Result<LocationIterator<'_>, FsError>>;
 }
